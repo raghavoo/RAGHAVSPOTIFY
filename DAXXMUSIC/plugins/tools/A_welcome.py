b83d1034c76fb278dfa854e1953db2e323be6c1e
raghavoo <155086740+raghavoo@users.noreply.github.com>
2024-03-06 16:05:44 +0530

    A_welcome.py

--- DAXXMUSIC/plugins/tools/A_welcome.py
+++ DAXXMUSIC/plugins/tools/A_welcome.py
@@ -1,4 +1,4 @@
-from VIPMUSIC import app
+from DAXXMUSIC import app
 from pyrogram import filters
 from pyrogram.errors import RPCError
 from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton
@@ -20,13 +20,13 @@ from pyrogram.enums import ParseMode
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-from VIPMUSIC.utils.vip_ban import admin_filter
+from DAXXMUSIC.utils.vip_ban import admin_filter
 import os
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-from VIPMUSIC.utils.database import get_assistant
+from DAXXMUSIC.utils.database import get_assistant
 
 random_photo = [
     "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
@@ -82,12 +82,12 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("VIPMUSIC/assets/wel2.png")
+    background = Image.open("DAXXMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((825, 824))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=110)
+    font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=110)
     welcome_font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=60)
     draw.text((2100, 1420), f'ID: {id}', fill=(12000, 12000, 12000), font=font)
     pfp_position = (1990, 435)
